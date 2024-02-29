b95473593746769e7133f2010207e88633b69eba
bandla.vignesh <vignesh.bandla01@gmail.com>
2024-02-29 19:08:00 +0530

    UPDATE : Added DB Script used

--- MakeProfits.DB/Scripts/UserLogin.sql
+++ MakeProfits.DB/Scripts/UserLogin.sql
@@ -1,145 +1,137 @@
-USE [master]
-GO
-/****** Object:  Database [MakeProfits]    Script Date: 26-02-2024 22:13:19 ******/
-CREATE DATABASE [MakeProfits]
- CONTAINMENT = NONE
- ON  PRIMARY 
-( NAME = N'MakeProfits', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.SQLEXPRESS\MSSQL\DATA\MakeProfits.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
- LOG ON 
-( NAME = N'MakeProfits_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.SQLEXPRESS\MSSQL\DATA\MakeProfits_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
- WITH CATALOG_COLLATION = DATABASE_DEFAULT, LEDGER = OFF
-GO
-ALTER DATABASE [MakeProfits] SET COMPATIBILITY_LEVEL = 160
-GO
-IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
-begin
-EXEC [MakeProfits].[dbo].[sp_fulltext_database] @action = 'enable'
-end
-GO
-ALTER DATABASE [MakeProfits] SET ANSI_NULL_DEFAULT OFF 
-GO
-ALTER DATABASE [MakeProfits] SET ANSI_NULLS OFF 
-GO
-ALTER DATABASE [MakeProfits] SET ANSI_PADDING OFF 
-GO
-ALTER DATABASE [MakeProfits] SET ANSI_WARNINGS OFF 
-GO
-ALTER DATABASE [MakeProfits] SET ARITHABORT OFF 
-GO
-ALTER DATABASE [MakeProfits] SET AUTO_CLOSE OFF 
-GO
-ALTER DATABASE [MakeProfits] SET AUTO_SHRINK OFF 
-GO
-ALTER DATABASE [MakeProfits] SET AUTO_UPDATE_STATISTICS ON 
-GO
-ALTER DATABASE [MakeProfits] SET CURSOR_CLOSE_ON_COMMIT OFF 
-GO
-ALTER DATABASE [MakeProfits] SET CURSOR_DEFAULT  GLOBAL 
-GO
-ALTER DATABASE [MakeProfits] SET CONCAT_NULL_YIELDS_NULL OFF 
-GO
-ALTER DATABASE [MakeProfits] SET NUMERIC_ROUNDABORT OFF 
-GO
-ALTER DATABASE [MakeProfits] SET QUOTED_IDENTIFIER OFF 
-GO
-ALTER DATABASE [MakeProfits] SET RECURSIVE_TRIGGERS OFF 
-GO
-ALTER DATABASE [MakeProfits] SET  DISABLE_BROKER 
-GO
-ALTER DATABASE [MakeProfits] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
-GO
-ALTER DATABASE [MakeProfits] SET DATE_CORRELATION_OPTIMIZATION OFF 
-GO
-ALTER DATABASE [MakeProfits] SET TRUSTWORTHY OFF 
-GO
-ALTER DATABASE [MakeProfits] SET ALLOW_SNAPSHOT_ISOLATION OFF 
-GO
-ALTER DATABASE [MakeProfits] SET PARAMETERIZATION SIMPLE 
-GO
-ALTER DATABASE [MakeProfits] SET READ_COMMITTED_SNAPSHOT OFF 
-GO
-ALTER DATABASE [MakeProfits] SET HONOR_BROKER_PRIORITY OFF 
-GO
-ALTER DATABASE [MakeProfits] SET RECOVERY SIMPLE 
-GO
-ALTER DATABASE [MakeProfits] SET  MULTI_USER 
-GO
-ALTER DATABASE [MakeProfits] SET PAGE_VERIFY CHECKSUM  
-GO
-ALTER DATABASE [MakeProfits] SET DB_CHAINING OFF 
-GO
-ALTER DATABASE [MakeProfits] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
-GO
-ALTER DATABASE [MakeProfits] SET TARGET_RECOVERY_TIME = 60 SECONDS 
-GO
-ALTER DATABASE [MakeProfits] SET DELAYED_DURABILITY = DISABLED 
-GO
-ALTER DATABASE [MakeProfits] SET ACCELERATED_DATABASE_RECOVERY = OFF  
-GO
-ALTER DATABASE [MakeProfits] SET QUERY_STORE = ON
-GO
-ALTER DATABASE [MakeProfits] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, CLEANUP_POLICY = (STALE_QUERY_THRESHOLD_DAYS = 30), DATA_FLUSH_INTERVAL_SECONDS = 900, INTERVAL_LENGTH_MINUTES = 60, MAX_STORAGE_SIZE_MB = 1000, QUERY_CAPTURE_MODE = AUTO, SIZE_BASED_CLEANUP_MODE = AUTO, MAX_PLANS_PER_QUERY = 200, WAIT_STATS_CAPTURE_MODE = ON)
-GO
-USE [MakeProfits]
-GO
-/****** Object:  Table [dbo].[Users]    Script Date: 26-02-2024 22:13:19 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Users](
-	[UserID] [int] NOT NULL,
-	[Name] [varchar](25) NOT NULL,
-	[UserName] [varchar](25) NOT NULL,
-	[Password] [varchar](50) NOT NULL,
- CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
-(
-	[UserID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  StoredProcedure [dbo].[RegisterUser]    Script Date: 26-02-2024 22:13:19 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE PROCEDURE [dbo].[RegisterUser] 
+DROP TABLE MakeProfits.Roles;
+DROP TABLE Advisors;
+DROP TABLE Agents;
+DROP TABLE Users;
+CREATE TABLE Users(
+	UserID int PRIMARY KEY,
+	RoleID int NOT NULL,
+	AddressLine nvarchar(75),
+	City nvarchar(50),
+	State nvarchar(50),
+	EmailAddress nvarchar(50),
+	AdvisorID int NOT NULL,
+	AgentID int NOT NULL,
+
+	FirstName nvarchar(50),
+	LastName nvarchar(50),
+	UserName nvarchar(50),
+	Password nvarchar(50),
+	PhoneNumber nvarchar(12),
+	Company nvarchar(75),
+
+	IsActive int,
+
+	CONSTRAINT CHL_ISACTIVE CHECK (IsActive =1 OR IsActive=0),
+	CONSTRAINT FK_users_Roles FOREIGN KEY (RoleID) REFERENCES Roles(RoleID),
+	CONSTRAINT FK_users_advisors FOREIGN KEY (AdvisorID) REFERENCES Advisors(AdvisorID),
+	CONSTRAINT FK_users_gent FOREIGN KEY (AgentID) REFERENCES Agents(AgentID)
+);
+
+CREATE TABLE Advisors(
+	AdvisorID int PRIMARY KEY,
+	AdvisorName nvarchar(50) NOT NULL
+);
+
+CREATE TABLE Agents(
+	AgentID int PRIMARY KEY,
+	AgentName nvarchar(50) NOT NULL
+);
+CREATE TABLE Roles(
+	RoleID int PRIMARY KEY,
+	RoleName nvarchar(50) NOT NULL
+);
+
+INSERT INTO Roles VALUES (1,'User');
+
+INSERT INTO Advisors VALUES (1,'DummyAdvisor');
+
+INSERT INTO Agents VALUES (1,'DummyAgent');
+SELECT * FROM Roles,Advisors,Agents;
+
+SELECT * FROM Users;
+
+
+CREATE PROCEDURE InsertUser 
 	-- Add the parameters for the stored procedure here
-	@UserName VARCHAR(25),
-    @Password VARCHAR(50),  
-    @Name VARCHAR(25),
-    @UserId INT
+	@UserID int = 0, --  
+	@RoleID int = 1, -- Default Rol
+	@AddressLine nvarchar(75),
+	@City nvarchar(50),
+	@State nvarchar(50),
+	@EmailAddress nvarchar(50),
+	@AdvisorID int = 1, -- Default Advisor
+	@AgentID int  = 1, -- Default Agent
+	@FirstName nvarchar(50),
+	@PhoneNumber nvarchar(12),
+	@LastName nvarchar(50),
+	@UserName nvarchar(50),
+	@Password nvarchar(50),
+	@Company nvarchar(75)
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
-	INSERT INTO Users (UserName, Password, Name, UserID)
-    VALUES (@UserName, @Password, @Name, @UserId)
+
+    -- Insert statements for procedure here
+	INSERT INTO Users(UserID,RoleID,AddressLine,City,State,EmailAddress,AdvisorID,AgentID,FirstName,LastName,UserName,Password,PhoneNumber,Company,IsActive) 
+	VALUES(@UserID,@RoleID,@AddressLine,@City,@State,@EmailAddress,@AdvisorID,@AgentID,@FirstName,@LastName,@UserName,@Password,@PhoneNumber,@Company,1);
+	
 END
-GO
-/****** Object:  StoredProcedure [dbo].[ValidateUser]    Script Date: 26-02-2024 22:13:19 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE PROCEDURE [dbo].[ValidateUser] 
+CREATE PROCEDURE GetUserByID
+	@UserID int
+AS
+BEGIN
+	
+SELECT r.RoleName
+			,u.AddressLine
+			,u.City
+			,u.State
+			,u.EmailAddress
+			,adv.AdvisorName
+			,a.AgentName
+			,u.FirstName
+			,u.LastName
+			,u.UserName
+			,u.PhoneNumber
+			,u.Company
+	FROM Users u,Agents a, Roles r, Advisors adv
+	WHERE u.UserID=@UserID AND a.AgentID=u.AgentID AND u.AdvisorID = adv.AdvisorID AND u.RoleID = r.RoleID 
+END
+CREATE PROCEDURE GetUserByName
+	@UserName nvarchar(50)
+AS
+BEGIN
+	
+SELECT u.FirstName
+			,u.LastName
+			,u.UserName
+			,u.PhoneNumber		
+			,u.AddressLine
+			,u.City
+			,u.State
+			,u.EmailAddress
+			,adv.AdvisorName
+			,a.AgentName
+			,u.Company
+			,r.RoleName
+	FROM Users u,Agents a, Roles r, Advisors adv
+	WHERE u.UserName=@UserName AND a.AgentID=u.AgentID AND u.AdvisorID = adv.AdvisorID AND u.RoleID = r.RoleID 
+END
+CREATE PROCEDURE ValidateUser
 	-- Add the parameters for the stored procedure here
-	@UserName VARCHAR(25),
+	@UserName VARCHAR(50),
     @Password VARCHAR(50)
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
-	SELECT CASE 
-		WHEN UserName = @UserName AND Password =@Password THEN 'true'
-		ELSE 'false'
+	SELECT  CASE 
+		WHEN UserName = @UserName AND Password =@Password THEN 1
+		ELSE 0
 	END AS IsValidUser
 	FROM Users
 END
-GO
-USE [master]
-GO
-ALTER DATABASE [MakeProfits] SET  READ_WRITE 
-GO
+
+
